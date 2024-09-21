d0a3b5851dee62a7433b53e27481ffec2d7b937c
vinodgaur1911 <vinodgaur1911@gmail.com>
2024-09-21 21:13:14 +0530

    initial commit

---
+++ app/src/main/java/com/tw/settingsscreendemo/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.tw.settingsscreendemo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //you can navigate to diff-diff screens on clicking each cardview
+        //like open Profile, Whatsapp service center on mobile, call, email etc.
+
+    }
+
+
+}
